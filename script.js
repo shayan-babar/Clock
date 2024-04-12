1d3c3141981a3e3af1a6bf8be672da490c4b48dc
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-04-13 02:08:20 +0500

    ⌚

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberHours = document.querySelector(".number-hours");
+const barSeconds = document.querySelector(".bar-seconds");
+
+const numberElement = [];
+const barElement = [];
+
+for (let i = 1; i <= 12; i++) {
+  numberElement.push(`<span style="--index:${i};"><p>${i}</p></span>`);
+}
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
+
+for (let i = 1; i <= 60; i++) {
+  barElement.push(`<span style="--index:${i};"><p></p></span>`);
+}
+barSeconds.insertAdjacentHTML("afterbegin", barElement.join(""));
+
+const handHours = document.querySelector(".hand.hours");
+const handMinutes = document.querySelector(".hand.minutes");
+const handSeconds = document.querySelector(".hand.seconds");

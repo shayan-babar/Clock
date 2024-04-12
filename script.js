e919967e87d5e37ce931ec9475d91e3f9849c4ec
Shayan <154625679+shayan-babar@users.noreply.github.com>
2024-04-13 02:09:10 +0500

    ⌚

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ barSeconds.insertAdjacentHTML("afterbegin", barElement.join(""));
 const handHours = document.querySelector(".hand.hours");
 const handMinutes = document.querySelector(".hand.minutes");
 const handSeconds = document.querySelector(".hand.seconds");
+
+function getCurrentTime() {
+  let date = new Date();
+  let currentHours = date.getHours();
+  let currentMinutes = date.getMinutes();
+  let currentSeconds = date.getSeconds();
+
+  handHours.style.transform = `rotate(${currentHours * 30 + currentMinutes / 2
+    }deg)`;
+  handMinutes.style.transform = `rotate(${currentMinutes * 6}deg)`;
+  handSeconds.style.transform = `rotate(${currentSeconds * 6}deg)`;
+}
+
+getCurrentTime();
+setInterval(getCurrentTime, 1000);
